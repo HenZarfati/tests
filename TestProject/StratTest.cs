cf1480b08d8d12d41372020b86efcad9d789233e
Hen Zarfati <Hen.zarfati5@gmail.com>
2021-04-23 17:00:11 +0300

    import files

---
+++ TestProject/StratTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class StratTest
+    {
+        public bool DoSomething()
+        {
+            int x = 6;
+            int y = x;
+            return true;
+        }
+    }
+}
